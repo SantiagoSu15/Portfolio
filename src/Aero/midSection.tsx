b1c824ea707808c65b2aab4c743eb6ea1dfb2ac9
SantiagoSu15 <sasupu08@gmail.com>
2026-08-21 09:47:14 -0500

    Elminacion de dependencia

--- src/Aero/midSection.tsx
+++ src/Aero/midSection.tsx
@@ -5,9 +5,9 @@ export const MidSection: React.FC = () => {
   return (
     <>
       <section className="text-center mb-16" data-purpose="hero-area">
-        <h1 className="text-6xl font-bold hero-text text-[#006080] mb-2">Projects</h1>
+        <h1 className="text-6xl font-bold hero-text text-[#006080] mb-2">Portfolio</h1>
         <p className="text-xl opacity-80 font-medium">
-          A list of personal and academic projects
+          My personal portfolio
         </p>
       </section>
 
